c58e60faa11fa59e579846a6172ddf6128ec482a
Abdur Rehman <abdurrehmananqayyum@gmail.com>
2026-01-18 03:17:48 +0500

    Logo link added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
 
 const activePage = ()  => {
     navLinks.forEach(link => {
@@ -14,6 +15,14 @@ navLinks.forEach((link, idx) => {
             link.classList.add('active');
         }
     });
+});
+
+logoLink.addEventListener('click', () => {
+    if (!navLinks[0].classList.contains('active')) {
+        activePage();
+
+        navLinks[0].classList.add('active');
+    }
 });
 
 const resumeBtns = document.querySelectorAll('.resume-btn');
